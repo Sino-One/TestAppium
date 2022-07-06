08b489bbee3335458bf931cfa9c92de50f9f91dd
Sinwan <sinwan.meynaud@cbainfo.fr>
2022-07-06 10:47:43 +0200

    fix AndroidUIAutomator and deprecated

--- src/testAppium/UiAutomatorTest.java
+++ src/testAppium/UiAutomatorTest.java
@@ -4,17 +4,20 @@ import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 
 public class UiAutomatorTest extends base {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws MalformedURLException {
 		AndroidDriver driver = capabilities();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-		driver.findElement(By.name("Views")).click();
+		driver.findElement(AppiumBy.androidUIAutomator("text(\"Views\")")).click();
+		
+		System.out.println(driver.findElement(AppiumBy.androidUIAutomator("newUiSelector().clickable(true)")).getSize());
+		
 
 	}
 
